0ec5de1173afa3cb826bf204c56906153b963e13
quehlman51 <33034326+quehlman51@users.noreply.github.com>
2018-05-07 03:23:37 -0500

    Created code

--- src/P6_Quadrant.java
+++ src/P6_Quadrant.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 	ISYS 320
 	Name(s): Anthony Quehl
@@ -7,7 +9,20 @@
 public class P6_Quadrant {
 
 	public static void main(String[] args) {
-		
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter your first number: ");
+		int num1 = console.nextInt();				
+		System.out.print("Enter your second number: ");
+		int num2 = console.nextInt();
+				if(num1 > 0 && num2 > 0 ) {
+					System.out.println("This number is in Quadrant 1");
+				}else if(num1 < 0 && num2 > 0) {
+					System.out.println("This number is in Quadrant 2");		
+				}else if(num1 < 0 && num2 < 0) {
+					System.out.println("This number is in Quadrant 3");
+				}else if(num1 > 0 && num2 < 0) {
+					System.out.println("This number is in Quadrant 4");
+				}
 	}
 
 }
